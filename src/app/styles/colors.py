af0dae7cbf1a0143391b4be081dc16f39880429f
4l3j0Ok <alejofsarmiento@gmail.com>
2024-05-06 02:05:49 -0300

    Added colors.py file with palette definitions

---
+++ src/app/styles/colors.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Palette(Enum):
+    background = "#0B0014"
+    accent = "#D44D5C"
+    white = "#F5E9E2"
+    dogwood = "#E3B5A4"
+    wine = "#773344"
+    color_scheme = "ruby"
